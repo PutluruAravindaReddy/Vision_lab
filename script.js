4a95c760f4b00f43714d795695de819e395244af
PutluruAravindaReddy <aravindareddy60@gmail.com>
2024-01-13 08:16:18 +0530

    Home page update

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+// hamburger=document.querySelector(".hamburger");
+// hamburger.onclick=function(){
+//   navBar = document.querySelector(".nav-bar");
+//   navBar.classList.toggle("active");
+// }
+
+var featuresclick = document.getElementsByClassName("pages");
+
+for (let i = 0; i < featuresclick.length; i++) {
+  featuresclick[i].addEventListener("click", function () {
+      var activef = document.getElementsByClassName("active");
+      for (let j = 0; j < activef.length; j++) {
+          activef[j].classList.remove("active");
+      }
+      this.classList.add("active");
+  });
+}
+
 // Script for navigation bar
 window.onscroll = function() {scrollFunction()};
 
@@ -13,3 +31,11 @@ function scrollFunction() {
     document.getElementById("navbar").classList.remove("scroll");
   }
 }
+
+
+let navbarsym=document.getElementById("mobilenav");
+let sidebar=document.getElementByTd("sidebar");
+sidebar.addEventListener("click",function(){
+  navbarsym.classList.toggle("none");
+  console.log("clicked");
+});
