124aa529743a9bbee5b7a14a5d4102983391879a
PutluruAravindaReddy <aravindareddy60@gmail.com>
2024-01-14 13:33:06 +0530

    Update

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// { Navbar js }
+
 document.addEventListener('DOMContentLoaded', function () {
     var sidebar = document.getElementById('sidebar');
     var navbar = document.getElementById('navbar2');
@@ -25,7 +27,6 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   });
 
-    // Initial setup
     window.dispatchEvent(new Event('resize'));
 });
 
@@ -44,4 +45,48 @@ function scrollFunction() {
     document.querySelector(".active").classList.remove("greenandwhite");
 
   }
+}
+
+// { faculty js }
+
+let empty=document.querySelectorAll(".empty");
+let students=document.querySelectorAll(".students");
+
+for(let i=0;i<students.length;i++){
+students[i].addEventListener('mouseover',function(){
+  empty[i].classList.add("colorbox");});
+
+students[i].addEventListener('mouseout',function(){
+  empty[i].classList.remove("colorbox");
+});
+}
+
+
+// { Research projects js }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activeslide", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activeslide";
 }
